410d53a47e9535f221878e5d255b33f772088a08
Zaryab <sohail.zaryab61@gmail.com>
2020-10-16 06:08:20 -0700

    comiiting

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-add-item-form',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemFormComponent implements OnInit {
 
+ @Input() item: BudgetItem = new BudgetItem('',null);
   constructor() { }
-
   ngOnInit() {
+    
+     
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 
 }
